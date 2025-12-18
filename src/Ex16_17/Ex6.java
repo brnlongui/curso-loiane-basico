61d76c2e43e6f0c6760f7af21725fd85b38f6e10
brnlongui <brunorlongui@gmail.com>
2025-12-17 23:40:53 -0400

    ADD exercicios

---
+++ src/Ex16_17/Ex6.java
@@ -0,0 +1,11 @@
+package Ex16_17;
+
+public class Ex6 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 21; i++) {
+            System.out.println(i);
+        }
+        System.out.print("1,2,3,4,5,6,7,8,9.10,11,12,13,14,15,16,17,18,19,20");
+    }
+}
---
+++ src/Ex16_17/Ex9.java
@@ -0,0 +1,12 @@
+package Ex16_17;
+
+public class Ex9 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 151; i++) {
+            if (i%2 != 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
